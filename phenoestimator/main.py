8f3de2d7f2ddd8d9b378fbb919934e6946316aa9
TWorm <tworm@ncsu.edu>
2025-05-23 13:39:17 -0400

    main scaffold

--- phenoestimator/main.py
+++ phenoestimator/main.py
@@ -1,5 +1,10 @@
 def main():
-    print("Hello from phenoestimator!")
+    # observations = get_inat_observations("Quercus rubra")
+    # weather = get_weather_data(observations)
+    # phenology = Phenology(observations, weather)
+    # phenology.plot()
+
+    print("foo")
 
 
 if __name__ == "__main__":
